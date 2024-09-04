f85c6fdc6386db9fca14a940926cd3ba0512913f
WidgetMasterLiam <150624032+WidgetMasterLiam@users.noreply.github.com>
2024-09-04 11:26:01 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -87,8 +87,7 @@ class Specialform extends HTMLElement {
       </div>
       <div class="para-form">
         <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+          with Lovely Solutions, LLC.
         </p>
       </div>
       <div class="input-fields">
@@ -114,7 +113,7 @@ class Specialform extends HTMLElement {
             type="text"
             name="idea"
             id="idea"
-            placeholder="Let's talk about your idea"
+            placeholder="Let's talk!"
             required
           />
           <div class="form-submit-button">
@@ -130,7 +129,11 @@ class Specialform extends HTMLElement {
     </div>
     <div class="first-form-para">
       <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+        Have an idea for something in the monday.com realm? <br>
+        <br>
+        We pride ourselves on hearing everyone out, and giving credit where it's due.<br>
+        <br>
+        Let's talk!
       </p>
     </div>
   </div>
